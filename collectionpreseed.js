aea8cc69be415794edfc897091a987aed8c3cf69
Mitra Ardron <mitra@mitra.biz>
2018-07-25 11:45:12 -0700

    Restore logging

--- collectionpreseed.js
+++ collectionpreseed.js
@@ -67,20 +67,20 @@ class Mirror {
             new s({name: "EatConfig"}).fromEdibleArray(Object.keys(config.collections))
                 .pipe(new s({uniq}).uniq())
 
-                //.pipe(new s().log((m) => ["Level1:", m])) - will display on MirrorCollectionSearchStream
+                .pipe(new s().log((m) => ["Level1 queueing", m])) //will display on MirrorCollectionSearchStream when processed
                 .pipe(new s({name: 'Create MirrorCollections 1'}).map((name) => new MirrorCollection({itemid: name}) ))  // Initialize collection - doesnt get metadata or search results
                 // Stream of ArchiveItems - which should all be collections
-                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 1", limit: 100, maxpages: 3, parallel, silentwait: true}))
+                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 1", limit: 100, maxpages: 3, parallel, silentwait: false}))
                 // Stream of arrays of Archive Items (mixed)
                 .pipe(new s({name: '1 split arrays of AI'}).split())
                 .pipe(new s({name: '1 filter by collection'}).filter((zz) => zz.mediatype === "collection"))
                 .pipe(new s({name: '1 identifier'}).map((xx) => xx.identifier))
                 .pipe(new s({uniq}).uniq())
 
-                //.pipe(new s().log((m) => ["Level2:", m])) - will display on MirrorCollectionSearchStream
+                .pipe(new s().log((m) => ["Level2 queueing", m])) //will display on MirrorCollectionSearchStream when processed
                 .pipe(new s({name: 'Create MirrorCollections 2'}).map((name) => new MirrorCollection({itemid: name}) ))  // Initialize collection - doesnt get metadata or search results
                 // Stream of ArchiveItems - which should all be collections
-                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 2", limit: 100, maxpages: 2, parallel, silentwait: true}))
+                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 2", limit: 100, maxpages: 2, parallel, silentwait: false}))
                 // Stream of arrays of Archive Items (mixed)
                 .pipe(new s({name: '2 split arrays of AI'}).split())
                 .pipe(new s({name: '2 filter by collection'}).filter((zz) => zz.mediatype === "collection"))
@@ -88,10 +88,10 @@ class Mirror {
                 .pipe(new s({uniq}).uniq())
 
 
-                //.pipe(new s().log((m) => ["Level3:", m])) - will display on MirrorCollectionSearchStream
+                .pipe(new s().log((m) => ["Level3 queueing:", m])) //will display on MirrorCollectionSearchStream when processed
                 .pipe(new s({name: 'Create MirrorCollections 3'}).map((name) => new MirrorCollection({itemid: name}) ))  // Initialize collection - doesnt get metadata or search results
                 // Stream of ArchiveItems - which should all be collections
-                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 3", limit: 100, maxpages: 1, parallel, silentwait: true}));
+                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 3", limit: 100, maxpages: 1, parallel, silentwait: false}));
                 //IGNORED Stream of arrays of Archive Items (mixed)
 
             let popularCollections = new MirrorSearch({
