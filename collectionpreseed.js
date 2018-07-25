7f75546dc366abd28586abc4cdb3f4e467e4e672
Mitra Ardron <mitra@mitra.biz>
2018-07-25 12:14:28 -0700

    Tweak timing

--- collectionpreseed.js
+++ collectionpreseed.js
@@ -70,7 +70,7 @@ class Mirror {
                 .pipe(new s().log((m) => ["Level1 queueing", m])) //will display on MirrorCollectionSearchStream when processed
                 .pipe(new s({name: 'Create MirrorCollections 1'}).map((name) => new MirrorCollection({itemid: name}) ))  // Initialize collection - doesnt get metadata or search results
                 // Stream of ArchiveItems - which should all be collections
-                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 1", limit: 100, maxpages: 3, parallel, silentwait: false}))
+                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 1", limit: 100, maxpages: 1, parallel, silentwait: false}))
                 // Stream of arrays of Archive Items (mixed)
                 .pipe(new s({name: '1 split arrays of AI'}).split())
                 .pipe(new s({name: '1 filter by collection'}).filter((zz) => zz.mediatype === "collection"))
@@ -80,7 +80,7 @@ class Mirror {
                 .pipe(new s().log((m) => ["Level2 queueing", m])) //will display on MirrorCollectionSearchStream when processed
                 .pipe(new s({name: 'Create MirrorCollections 2'}).map((name) => new MirrorCollection({itemid: name}) ))  // Initialize collection - doesnt get metadata or search results
                 // Stream of ArchiveItems - which should all be collections
-                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 2", limit: 100, maxpages: 2, parallel, silentwait: false}))
+                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 2", limit: 60, maxpages: 2, parallel, silentwait: false}))
                 // Stream of arrays of Archive Items (mixed)
                 .pipe(new s({name: '2 split arrays of AI'}).split())
                 .pipe(new s({name: '2 filter by collection'}).filter((zz) => zz.mediatype === "collection"))
@@ -91,17 +91,25 @@ class Mirror {
                 .pipe(new s().log((m) => ["Level3 queueing:", m])) //will display on MirrorCollectionSearchStream when processed
                 .pipe(new s({name: 'Create MirrorCollections 3'}).map((name) => new MirrorCollection({itemid: name}) ))  // Initialize collection - doesnt get metadata or search results
                 // Stream of ArchiveItems - which should all be collections
-                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 3", limit: 100, maxpages: 1, parallel, silentwait: false}));
+                .pipe(new MirrorCollectionSearchStream({name: "Collection Preseed level 3", limit: 30, maxpages: 1, parallel, silentwait: false}));
                 //IGNORED Stream of arrays of Archive Items (mixed)
+
+            new s({name: "EatPopularCollections"}).fromEdibleArray([popularCollections])
+                .pipe(new s({uniq}).uniq())
+                .pipe(new MirrorCollectionSearchStream({name: "Collection popular search", limit: 300, maxpages: 1, parallel, silentwait: false}))
+                // Stream of arrays of Archive Items (mixed)
+                .pipe(new s({name: '1 split arrays of AI'}).split())
+                .pipe(new s({name: '1 filter by collection'}).filter((zz) => zz.mediatype === "collection"))
+                .pipe(new s({name: '1 identifier'}).map((xx) => xx.identifier))
+                .pipe(new s({uniq}).uniq())
+                .pipe(new MirrorCollectionSearchStream({name: "Collection Popular", limit: 100, maxpages: 1, parallel, silentwait: false}))
+            // No need to do something with these
 
             let popularCollections = new MirrorSearch({
                 query: 'mediatype:collection AND NOT _exists_:access-restricted',
                 sort: '-downloads',
             });
 
-            new s({name: "EatPopularCollections"}).fromEdibleArray([popularCollections])
-                .pipe(new s({uniq}).uniq())
-                .pipe(new MirrorCollectionSearchStream({limit: 100, maxpages: 3, parallel, silentwait: true}))
         } catch(err) {
             console.error(err);
         }
