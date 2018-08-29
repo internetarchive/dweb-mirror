1e1a862982abcebb5470550364dac0e15710ab92
Mitra Ardron <mitra@mitra.biz>
2018-08-29 13:01:54 +1000

    Install.sh - added some stubs for IPFS

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# TODO install IPFS go daemon
+#
+if ! (ipfs --version)
+then
+    echo "Need to install IPFS from "https://dist.ipfs.io/#go-ipfs"
+    open https://dist.ipfs.io/#go-ipfs
+    # TODO install IPFS go daemon automagically
+else
+    ipfs config --json Experimental.FilestoreEnabled true
+fi
+
 # TODO update from GIT - using template from previous installtransport.sh
 
 # TODO add docker files as alternative ?
