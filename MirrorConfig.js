92f95df32c8b972102e211f4cb543d0b487cc17d
Mitra Ardron <mitra@mitra.biz>
2019-05-26 19:57:08 +1000

    Update default User Config - its not copied from dweb-mirror.config.yaml

--- MirrorConfig.js
+++ MirrorConfig.js
@@ -90,9 +90,11 @@ class MirrorConfig extends ConfigController { //TODO-API split from ConfigContro
     }
 }
 MirrorConfig.userConfigFile =   "~/dweb-mirror.config.yaml"; // contents overwritten by writeUser or setAndWriteUser
-MirrorConfig.defaultUserConfig = {apps: { crawl: { tasks: [] }}};
+// Default to just top 30 tiles of home page
+MirrorConfig.defaultUserConfig = {apps: {crawl: { tasks: { identifier: [ "home"], level: "details", search:[{sort: "-downloads", rows: 30, level: "tile"}]}}}};
 // config files (later override earlier) note the userConfigFile is always appended
 // If this is ever more than one file in defaultConfigFiles then the code in dweb-archive that for statusFromConfig will need editing as assumes userConfigFile returned in position 1
 MirrorConfig.defaultConfigFiles = [ "./configDefaults.yaml"];
 
 exports = module.exports = MirrorConfig;
+
